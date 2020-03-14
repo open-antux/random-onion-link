3ec8457866852802fac9ba61a56e18186894d969
Superjolly002 <antonio.giustino11@gmail.com>
2020-03-14 12:28:16 +0100

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function init(){
+  // TODO: Seed the random function
+}
+
 function generateLink(){
   var charset = "qwertyuiopasdfghjklzxcvbnm1234567890";
   var link    = "";
@@ -8,6 +12,21 @@ function generateLink(){
 
   link += ".onion";
 
-  document.getElementById('link').innerText = link;
+  document.getElementById("link").innerText = link;
 
+}
+
+function copyIntoClipboard(){
+  var range = document.createRange();
+
+  range.selectNode(document.getElementById("link"));
+  window.getSelection().addRange(range);
+
+  document.execCommand("copy");
+}
+
+function openNewTab(){
+  var link = document.getElementById("link");
+
+  window.open(link, "_blank").focus();
 }
